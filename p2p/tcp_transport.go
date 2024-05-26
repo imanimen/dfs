cefb87dd902403136b8e892124ee8b3cd19ec09f
iman <imanimen81@gmail.com>
2024-05-26 12:04:27 +0330

    add: listenAndAccept with tests

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -1,6 +1,7 @@
 package p2p
 
 import (
+	"fmt"
 	"net"
 	"sync"
 )
@@ -14,8 +15,40 @@ type TCPTransport struct {
 
 }
 
+// NewTCPTransport creates a new TCPTransport instance with the provided listen address.
+// The TCPTransport is responsible for managing the TCP connection for the p2p network.
 func NewTCPTransport(listenAddr string) *TCPTransport {
 	return &TCPTransport{
 		ListenAddress: listenAddr,
 	}
+}
+
+func (t *TCPTransport) ListenAndAccept() error {
+	var err error
+
+	t.Listener, err = net.Listen("tcp", t.ListenAddress)
+	if err != nil {
+		return err
+	}
+
+	go t.startAcceptLoop()
+	return nil
+
+}
+
+
+func (t *TCPTransport) startAcceptLoop() {
+	for {
+		conn, err := t.Listener.Accept()
+		if err != nil {
+			fmt.Printf("dfs: TCP accept error: %s\n", err)
+		}
+
+		go t.handleConn(conn)
+	}
+}
+
+
+func (t *TCPTransport) handleConn(conn net.Conn) {
+	fmt.Printf("dfs: new incoming connection %+v\n", conn)
 }
--- p2p/tcp_transport_test.go
+++ p2p/tcp_transport_test.go
@@ -10,4 +10,7 @@ func TestTCPTransport(t *testing.T) {
 	listenAddr := ":4000"
 	tr := NewTCPTransport(listenAddr)
 	assert.Equal(t, tr.ListenAddress, listenAddr)
+
+	// Server
+	assert.Nil(t, tr.ListenAndAccept())
 }
