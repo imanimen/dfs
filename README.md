8cb0e3107b1ca1adf55ec95046e352d0d30d6f8e
iman <imanimen81@gmail.com>
2024-05-26 11:50:01 +0330

    add: p2p module & test for tcp peer to peer connection

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 - This is a personal project
 - Daily Commit is required
 - Learn & Grow
+
+# Goals
+- Hash File
+- File Versioning
+- File Encryption
+- File Decryption
+- Cache & Index System
+
+# Dir
---
+++ p2p/tcp_transport.go
@@ -0,0 +1,21 @@
+package p2p
+
+import (
+	"net"
+	"sync"
+)
+
+type TCPTransport struct {
+	ListenAddress  string
+	Listener 	   net.Listener
+
+	mu 			   sync.RWMutex
+	peers 		   map[net.Addr]Peer
+
+}
+
+func NewTCPTransport(listenAddr string) *TCPTransport {
+	return &TCPTransport{
+		ListenAddress: listenAddr,
+	}
+}
