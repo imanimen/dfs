a9f654ef582c143bf2607e69b12bf8c70d4c86b4
iman <imanimen81@gmail.com>
2024-06-03 23:03:28 +0330

    fix: delete file:

--- store_test.go
+++ store_test.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"bytes"
-	"fmt"
-	"io"
+	// "fmt"
+	// "io"
 	"testing"
 )
 
@@ -12,32 +12,51 @@ import (
 // 	pathKey := CASPathTransformFunc(key)
 // 	expectedOriginalKey := "6804429f74181a63c50c3d81d733a12f14a353ff"
 // 	expectedPathName := "68044/29f74/181a6/3c50c/3d81d/733a1/2f14a/353ff"
-// 	// if pathKey.PathName != expectedPathName {
-// 	// 	t.Errorf("have %s want %s", pathKey.PathName, expectedPathName)
-// 	// }
-// 	// if pathKey.FileName != expectedPathName {
-// 	// 	t.Errorf("have %s want %s", pathKey.FileName, expectedOriginalKey)
-// 	// }
+// 	if pathKey.PathName != expectedPathName {
+// 		t.Errorf("have %s want %s", pathKey.PathName, expectedPathName)
+// 	}
+// 	if pathKey.FileName != expectedPathName {
+// 		t.Errorf("have %s want %s", pathKey.FileName, expectedOriginalKey)
+// 	}
 // }
-func TestStore(t *testing.T) {
+
+func TestStoreDeleteKey(t *testing.T) {
 	opts := StoreOpts{
 		PathTransformFunc: CASPathTransformFunc,
 	}
 	s := NewStore(opts)
-	key := "momsbestpicture"
+	key := "momsspecials"
 	data := []byte("some jpg bytes")
-	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
-		t.Error(err)
-	}
-	r, err := s.Read(key)
-	if err != nil {
+	if err := s.WriteStream(key, bytes.NewReader(data)); err != nil {
 		t.Error(err)
 	}
 
-	b, _ := io.ReadAll(r)
-	fmt.Println(string(b))
-	if string(b) != string(data) {
-		t.Errorf("want %s have %s", data, b)
+	err := s.Delete(key)
+	if err != nil {
+		return
 	}
 
 }
+
+// func TestStore(t *testing.T) {
+// 	opts := StoreOpts{
+// 		PathTransformFunc: CASPathTransformFunc,
+// 	}
+// 	s := NewStore(opts)
+// 	key := "momsbestpicture"
+// 	data := []byte("some jpg bytes")
+// 	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+// 		t.Error(err)
+// 	}
+// 	r, err := s.Read(key)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+
+// 	b, _ := io.ReadAll(r)
+// 	fmt.Println(string(b))
+// 	if string(b) != string(data) {
+// 		t.Errorf("want %s have %s", data, b)
+// 	}
+
+// }
